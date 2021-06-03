8a8590cad8af1683a2ccb235c973414934b8255d
ku zakhar <nik.name.doker@gmail.com>
2021-06-03 08:22:18 +0200

    f2 does not works

--- Source.cpp
+++ Source.cpp
@@ -20,165 +20,34 @@ Example of log2.txt:
 
 
 
-//
-//class Log {
-//
-//
-//public :
-//    Log(){}
-//    int a;
-//    string message;
-//
-//};
-//
-//
-//class A {
-//public:
-//    A();
-//   static Log a;
-//  
-//    
-//  
-//    
-//    void set_message(std::string mes) 
-//    {
-//        std::cout << "Log to: " << mes<< std::endl;
-//    }
-//
-//
-//
-//
-//    A &operator << (std::string msg) 
-//    {
-//   
-//      a.message += msg;
-//      return *this;
-//    }
-//
-//    A &operator () (std::string aa)
-//    {
-//        
-//      a.message += aa;
-//        return *this;
-//    }
-//
-//    A & operator () (int aa) 
-//    {
-//        
-//        a.message += std::to_string(aa);
-//        return *this;
-//
-//    }
-//
-//   
-//};
-//
-//
-//Log a;
-//
-//
-//
-//A::A(){
-//
-//}
-//class B:public A {
-// 
-//private:
-//    //Log a;
-//
-//public:
-//
-//    B() {  std::cout << "Construktor B"<< std::endl; };
-//  
-//   
-//};
-//
-//class C :public A {
-//
-//public:
-//    C(){ std::cout << "Construktor C with parametres"<< std::endl; };
-// 
-//
-//};
-//
-//B &get() {
-//
-//    B b;
-//    return b;
-//}
-//C &get(std::string) {
-//
-//    C c;
-//    return c;
-//}
-//
-//
-//
-//C& get(std::string, std::string) {
-//
-//    C c;
-//    return c;
-//}
 
-class ConsoleLog {
 
-};
-
-class FileLog {
-
-};
-#include < vector >
-enum class LogType {
-    console, file
-};
-class WrapperLog {
-public:
-    OneLog& operator()(int status) {
-        saveAll();
-        container.push_back( OneLog( status ) );
-        return container.back();
-    }
-    void saveAll() {
-        if (logType == LogType::console)
-            saveToConsole();
-        else if(logType == LogType::file)
-            saveToFile();
-
-        container.clear();
-    }
-    ~WrapperLog() {
-        saveAll();
-    }
-private:
-    void saveToConsole();
-    void saveToFile();
-
-    LogType logType;
-    std::optional< OneLog > container;//std::optional
-};
 void f1() {
-  
 
-
-   auto consoleLogger = getLogger(); // Log to console
-   
-   // Console output: 23.02.2020 18:06:06; DEBUG; (2561): Running a thread
-  consoleLogger(DEBUG) << "Running a thread";
- 
-    auto fileLogger = getLogger("log.txt"); // Log to file with name log.txt
-    //// File output: 23.02.2020 18:06:06; INFO; (2561): Running a thread
-    fileLogger << "Running a thread"; // Default log level INFO
+       auto consoleLogger = getLogger(); // Log to console
+       
+       // Console output: 23.02.2020 18:06:06; DEBUG; (2561): Running a thread
+        consoleLogger(DEBUG) << "Running a thread";
+     
+        auto fileLogger = getLogger("log.txt"); // Log to file with name log.txt
+        ////// File output: 23.02.2020 18:06:06; INFO; (2561): Running a thread
+        fileLogger << "1Running a thread"; // Default log level INFO
+    
 }
+
+
+
+
+
 
 void f2() {
     auto logger = getLogger("log.txt", "f2"); // Log to file with name log.txt. Each message has f2 prefix
-    // File output: 23.02.2020 18:06:06; DEBUG; f2(3444): Running a thread
-    logger(DEBUG) << "Running a thread";
-
+     //File output: 23.02.2020 18:06:06; DEBUG; f2(3444): Running a thread
+    logger(DEBUG) << "2Running a thread";
 
     double timeSpent = 10.0;
-    // File output: 23.02.2020 18:06:06; WARNING; f2(3444): Time spent in the thread: 10.0 seconds
-    logger(WARNING) << "Time spent in the thread: " << timeSpent << " seconds";
+    //File output: 23.02.2020 18:06:06; WARNING; f2(3444): Time spent in the thread: 10.0 seconds
+    logger(WARNING) << "Time spent in the thread: ";//; << timeSpent << " seconds";
 }
 
 
@@ -186,15 +55,13 @@ void f2() {
 void f3() {
     auto logger = getLogger("log2.txt", "f3");  // Log to file with name log2.txt. Each message has f3 prefix
     // File output: 23.02.2020 18:06:06; INFO; f3(3444): Running a thread
-    logger << "Running a thread"; // Default log level INFO
-
-
+    logger << "3Running a thread"; // Default log level INFO
 
     int i = 123;
     // File output: 23.02.2020 18:06:06; ERROR; f3(3444): My int is 123;
-    logger(ERROR) << "My int is " << i;
+    logger(ERROR) << "My int is ";
+ //}
 }
-
 
 
 
@@ -205,59 +72,23 @@ void f3() {
 
 
 int main() {
-
-    //auto logger = getLogger(); // Log to console
+   auto logger = getLogger(); // Log to console
     //// Console output: 23.02.2020 18:06:06; INFO; (1020): Starting the app
-    //logger << "Starting the app"; // Default log level INFO
-
-
-
-   // auto masakra = get();
-
-
-
-    //cout << getTimeInString();
-
-
-
-
-
-    // current date/time based on current system
-
+   logger << "Starting the app"; // Default log level INFO
 
     try
     {
-
-
-
-
     std::thread t1(f1);
     std::thread t2(f2);
     std::thread t3(f3);
-
-
-
 
     t1.join();
     t2.join();
     t3.join();
 
-
-
-
     }
     catch (...) {
-       
         throw;
-
-
-
-
     }
-
-
-
-    std::cout << "sd";
-
     return 0;
 }
--- log.h
+++ log.h
@@ -4,13 +4,17 @@
 #include <string>
 #include <string_view>
 #include <ctime>
+#include < vector >
+#include <optional>
+#include<mutex>
+
+
 #define DEBUG 0
 #define WARNING 1
 #define ERROR 2
 #define INFO 3
 
-
-
+std::mutex mutex;
 
 
 
@@ -18,95 +22,253 @@ class OneLog {
 public:
 
 
-	OneLog();
-	~OneLog();
+	OneLog(const OneLog& A){
+	
+
+		message = A.message;
+		add = A.add;
+		additional = A.additional;
+		prefix = A.prefix;
+		status = A.status;
+		threadID = A.threadID;
+	
+	}
+
+
+	OneLog(){
+		stat = false;
+		this->status.first = "INFO", this->status.second = 4;
+		threadID = std::this_thread::get_id();
+
+	}
+	~OneLog(){
+	
+	}
 
 	void setThreadID(int id);
 	size_t getThreadID();
 	
-	std::string getTime();
+	std::string getTime()
+	{
+		time_t now = time(0);
+		tm ltm;
+		localtime_s(&ltm, &now);
 
-	void setPrefix(std::string strView);
-	std::string getPrefix();
-	void setMessage(std::string message);
-	std::string getMessage();
-	void setStatus(std::string status);
-	std::string getStatus();
+		return std::to_string(1900 + ltm.tm_year) + " " + std::to_string(1 + ltm.tm_mon) + " " + std::to_string(ltm.tm_mday) +
+			" " + std::to_string(ltm.tm_hour) + ":" + std::to_string(ltm.tm_min + 1) + ":" + std::to_string(ltm.tm_sec + 1);
+
+	}
+
+	void setPrefix(std::string prefix) {
+		this->prefix = prefix;
+	}
+	std::string getPrefix(){
+		return this->prefix;
+	}
+
+
+	void setMessage(std::string message) {
+
+		this->message = message;
+	}
+	std::string  getMessage() {
+	return this-> message;
+	}
+	void setStatus(int status) {
+
+
+		if (status == DEBUG)
+			this->status.first = "Debug", this->status.second = 1;
+		if (status == WARNING)
+			this->status.first = "Warning", this->status.second = 2;
+		if (status == ERROR)
+			this->status.first = "Error", this->status.second = 3;
+		if (status == INFO)
+			this->status.first = "INFO", this->status.second = 4;			\
+
+
+	}
+	int getStatus() {
+	
+		return status.second;
+	}
+	
+	OneLog& operator<<(double a) {
+
+		return *this;
+	}
+
+	OneLog& operator<<(std::string msg) {
+		
+		
+		this->message = msg;
+
+		return *this;
+	}
+
+	OneLog& operator<<(int add) {
+		stat = true;
+		this->add.emplace(add);
+		return *this;
+	}
+
+
+
+	std::string getLog() {
+
+		std::ostringstream ss;
+		ss << std::this_thread::get_id();
+		std::string  idstr = ss.str();
+
+		if (add.has_value()&&prefix!="") {
+			return getTime() + ":" + status.first + ":" + prefix + ":" + idstr + ":" + message + ":" + std::to_string(add.value()) + ";\n";
+		}
+		
+			
+		else
+		{
+		return getTime() + ":" + status.first + ":" + idstr + ":" + message + ";\n";
+		}
+	
+	}
+
+	bool stat;
 
 protected:
-	 std::string message;
-	 std::string prefix;
-	 std::string status;
-	 int id;
+	std::string  message;
+	std::optional<int>add;
+	 double additional;
+	 std::string  prefix;
+	 std::pair< std::string, int> status;
+	 std::thread::id threadID;
 	
 	//....date, prefix, status, sufix
 };
 
 
 
-class BaseLog {
 
 
-private:
-
-public:
 
 
-	virtual BaseLog& operator<<(std::string msg) =0;
-	virtual BaseLog& operator<<(int)=0;
-	virtual BaseLog& operator()(int)=0;
-	virtual void operator()(std::string)=0;
 
 
+
+enum class LogType {
+	console, file
 };
 
 
 
-class ConsoleLog: public BaseLog {
+class WrapperLog {
 
-private:
-  OneLog onelog;
 public:
-	ConsoleLog();
-	ConsoleLog& operator<<(std::string msg) override;
-    ConsoleLog& operator<<(int) override;
-	ConsoleLog& operator()(int) override;
-	void operator()(std::string)override;
-	void LogToConsole();
-};
 
 
 
+	WrapperLog(WrapperLog& a){
+		logType = a.logType;
+		path = a.path;
+		container = a.container;
+	}
 
-class FileLog: public BaseLog{
+	WrapperLog(){
+		if (!container.has_value()) {
+			container = OneLog();
+		}
 
-private :
-	OneLog onelog;
+		operator_scop = false;
+
+	}
+
+	OneLog& operator<<(std::string msg) {
+		
+		container.value().setMessage(msg);
+		return container.value();
+	}
+
+	OneLog& operator()(int status) {
+		
+		container.value().setStatus(status);
+		container.value().stat = true;
+		return container.value();
+
+	}
+	void saveAll() {
+		
+		if (logType == LogType::console)
+			saveToConsole();
+		else if (logType == LogType::file)
+			saveToFile(path);
+	
+	}
+	~WrapperLog() {
+
+		if (container.value().getStatus() != INFO) {
+			saveAll();
+		}
+		
+	}
+
+	bool operator_scop;
+	LogType logType;
+	std::optional<OneLog> container;
 	std::string path;
-	std::string perfix;
-	FileLog();
-
-public:
-	FileLog(std::string);
-	FileLog(std::string, std::string);
 
 
-    FileLog& operator<<(std::string msg) override;
-	FileLog& operator<<(int) override;
-	FileLog& operator()(int) override;
-	void operator()(std::string)override;
+private:
+static std::mutex mutex;
+	
+	void saveToConsole() {
+		if(container.value().getMessage() == "") {
+			return;
+		}
 
-	void LogToFile();
+		mutex.lock();
+		std::cout << container.value().getLog();
+		mutex.unlock();
+	}
+	void saveToFile(std::string path){
+		if (container.value().getMessage() == "") {
+			return;
+		}
+		mutex.lock();
+		std::ofstream MyFile(path, std::ios::out|std::ios::app );
+		MyFile << container.value().getLog();
+		mutex.unlock();
+	}
 };
 
 
 
+std::mutex WrapperLog::mutex;
+
+WrapperLog getLogger() {
+	
+	WrapperLog w;
+	w.logType = LogType::console;
+	return w;
+}
 
 
-ConsoleLog& getLogger();
+WrapperLog getLogger(std::string path) {
 
-FileLog& getLogger(std::string);
+	WrapperLog w;
+	w.path = path;
+	w.logType = LogType::file;
+	return w;
 
-FileLog& getLogger(std::string, std::string );
+
+}
 
 
+WrapperLog getLogger(std::string path, std::string perfix) {
+
+	WrapperLog w;
+	w.path = path;
+	w.container.value().setPrefix(perfix);
+	w.logType = LogType::file;
+
+	return w;
+
+}
